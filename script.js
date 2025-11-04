d77538e4b09253ff1faf194976e50af67cc28e47
AhmadFreeMan <ahmadreza.mirzaei03@gmail.com>
2025-11-04 17:01:15 +0330

    add: rolling dice & updating current score logic

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
+const currnet0El = document.getElementById("current--0");
+const currnet1El = document.getElementById("current--1");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
@@ -12,6 +14,8 @@ const btnHold = document.querySelector(".btn--hold");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+let currnetScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -22,4 +26,12 @@ btnRoll.addEventListener("click", function () {
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
 
+  // 3. check for rolled 1: if ture, switch to next player
+  if (dice !== 1) {
+    // Add dice to currnet score
+    currnetScore += dice;
+    currnet0El.textContent = currnetScore; // Change later: now is testing
+  } else {
+    // switch to next player
+  }
 });

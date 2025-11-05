223d834b6176df28d034af978ad1e0b804315c07
AhmadFreeMan <ahmadreza.mirzaei03@gmail.com>
2025-11-05 13:28:29 +0330

    add: toggle active player logic (switch active class)

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // Slecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
@@ -37,7 +39,9 @@ btnRoll.addEventListener("click", function () {
   } else {
     // switch to next playe
     document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
     currnetScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });

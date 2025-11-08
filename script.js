7ab060b09b1dc296e40f5ed9922427f5a70f9a30
AhmadFreeMan <ahmadreza.mirzaei03@gmail.com>
2025-11-08 18:42:33 +0330

    add: basic logic for new game — reset CSS winning and active player states

--- script.js
+++ script.js
@@ -78,3 +78,13 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click", function () {
+  // New game btn
+  playing = true;
+  activePlayer = 0;
+  player0El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--winner");
+  player1El.classList.remove("player--active");
+});
